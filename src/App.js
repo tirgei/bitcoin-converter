e25a8d357a63d7984581caec6d61d181285ebcd1
Vincent Tirgei <tirgeic@gmail.com>
2020-12-06 16:34:43 +0300

    Implement function to fetch latest BTC price and save to state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [btcPrice, setBtcPrice] = useState(null);
+
+  useEffect(() => {
+    fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd')
+    .then(res => res.json())
+    .then(
+      (response) => {
+        if(response) {
+          setBtcPrice(response['bitcoin']['usd']);
+        }
+        setIsLoading(false);
+      },
+      (error) => {
+        console.log(`Error loading BTC Price: ${error}`)
+        setIsLoading(false);
+      }
+    );
+  }, [])
+
+  return isLoading ? Loading() : Converter();
+}
+
+// This displays the actual converter
+function Converter() {
   return (
-    <div className="App">
+    <div className="App Center">
       <span id="app-name">BTC2USD Converter</span>
 
       <form className="Converter" >
@@ -13,6 +39,13 @@ function App() {
       </form>
     </div>
   );
+}
+
+// This shows the loading state
+function Loading() {
+  return (
+    <span className="Center"> Loading... </span>
+  )
 }
 
 export default App;
