a15b31c295772621fd1a6db2c7bc569001b90881
Vincent Tirgei <tirgeic@gmail.com>
2020-12-06 15:52:31 +0300

    Create input form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span id="app-name">BTC2USD Converter</span>
+
+      <form className="Converter" >
+        <label>BTC</label>
+        <input type="number" min="0"/>
+        <label>USD</label>
+        <input type="number" min="0"/>
+      </form>
     </div>
   );
 }
