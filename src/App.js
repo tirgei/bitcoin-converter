3fe62dac7dbf18139071a7f00687a5c3bf063032
Vincent Tirgei <tirgeic@gmail.com>
2020-12-07 10:45:24 +0300

    Implement function to calculate USD/BTC on user input

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
-  const [btcPrice, setBtcPrice] = useState(null);
+  const [btcPrice, setBtcPrice] = useState(0);
 
   useEffect(() => {
     fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd')
@@ -22,20 +22,50 @@ function App() {
     );
   }, [])
 
-  return isLoading ? Loading() : Converter();
+  return isLoading ? <Loading/> : <Converter btcPrice={btcPrice}/>;
 }
 
 // This displays the actual converter
-function Converter() {
+function Converter(props) {
+
+  const [convertedUsd, setConvertedUsd] = useState(null);
+  const [convertedBtc, setConvertedBtc] = useState(null);
+
+  const updateCurrency = (focused, event) => {
+    const input = event.target.value ?? 0;
+
+    /**
+     * Update based on input that is in focus
+     * 0 -> BTC Input
+     * 1 -> USD input
+     */
+    if (focused === 0) {
+      setConvertedBtc(input);
+      setConvertedUsd(input * props.btcPrice);
+    } else {
+      setConvertedUsd(input);
+      setConvertedBtc(input / props.btcPrice);
+    }
+  }
+
   return (
     <div className="App Center">
       <span id="app-name">BTC2USD Converter</span>
 
       <form className="Converter" >
         <label>BTC</label>
-        <input type="number" min="0"/>
+        <input 
+          type="number" 
+          value={convertedBtc || ''}
+          onChange={(event) => updateCurrency(0, event)}
+        />
+
         <label>USD</label>
-        <input type="number" min="0"/>
+        <input 
+          type="number" 
+          value={convertedUsd || ''}
+          onChange={(event) => updateCurrency(1, event)}
+        />
       </form>
     </div>
   );
